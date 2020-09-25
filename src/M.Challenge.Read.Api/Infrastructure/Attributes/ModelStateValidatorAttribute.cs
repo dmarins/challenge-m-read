b7becf1f6459a43ffeffbd4b8828bff285ca4cd1
Diogo da Silva França Marins <dmarins@braspag.com.br>
2020-09-24 22:54:43 -0300

    removendo partes desnecessárias

--- src/M.Challenge.Read.Api/Infrastructure/Attributes/ModelStateValidatorAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-
-namespace M.Challenge.Read.Api.Infrastructure.Attributes
-{
-    [ExcludeFromCodeCoverage]
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public class ModelStateValidatorAttribute : Attribute { }
-}
